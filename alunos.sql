e3e7b3cbf3d75a7bc84b5939de0fcbb1a59799dd
Ussama <ussamedeiros@gmail.com>
2025-02-25 16:14:19 +0000

    insert

--- alunos.sql
+++ alunos.sql
@@ -8,3 +8,20 @@ CREATE TABLE alunos (
     email VARCHAR(255) NOT NULL UNIQUE,
     idade INT NOT NULL
 );
+INSERT INTO alunos (nome, email, idade) VALUES
+
+('João Silva', 'joao.silva@example.com', 18),
+('Maria Santos', 'maria.santos@example.com', 17),
+('Pedro Oliveira', 'pedro.oliveira@example.com', 19),
+('Ana Costa', 'ana.costa@example.com', 18),
+('Carlos Ferreira', 'carlos.ferreira@example.com', 17),
+('Mariana Lima', 'mariana.lima@example.com', 19),
+('Lucas Rodrigues', 'lucas.rodrigues@example.com', 18),
+('Laura Souza', 'laura.souza@example.com', 17),
+('Fernando Santos', 'fernando.santos@example.com', 19),
+('Isabela Martins', 'isabela.martins@example.com', 18),
+('Rafaela Oliveira', 'rafaela.oliveira@example.com', 17),
+('Gustavo Lima', 'gustavo.lima@example.com', 19),
+('Camila Costa', 'camila.costa@example.com', 18),
+('Diego Ferreira', 'diego.ferreira@example.com', 17),
+('Larissa Rodrigues', 'larissa.rodrigues@example.com', 19);
